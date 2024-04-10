cc2ce4446dbbe43edbaec438a1af8dd61aa0d75d
Gediel <gedielcassemiro@gmail.com>
2024-04-10 20:58:14 -0300

    operacoes

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -5,7 +5,18 @@ import '../components/Button.css'
 import Button from "../components/Button.jsx"
 import Display from "../components/Display.jsx"
 
+ const initialState = {
+    displayValue: "0",
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+ }
+
 export default class Calculator extends Component {
+
+    state = { ...initialState }
+
     constructor(props) {
         super(props)
         this.clearMemory = this.clearMemory.bind(this)
@@ -14,21 +25,43 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        console.log('limpar')
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
-        console.log(operation)
+       if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true})
+       } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+       }
     }
 
     addDigit(n) {
-        console.log(n)
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+        
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false})
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
+        }
     }
 
     render() {
         return (
             <div className="calculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label='AC' click={this.clearMemory} triple/>
                 <Button label='/' click={this.setOperation} operation/>
                 <Button label='7' click={this.addDigit}/>
